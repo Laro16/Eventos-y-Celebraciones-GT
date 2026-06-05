79d2bc42ae72982263a25bf90f83a9965936d09f
Laro16 <luis21aro@gmail.com>
2026-06-05 17:55:54 -0600

    Delete render.js

--- render.js
+++
@@ -1,8 +0,0 @@
-// render.js
-const rootEl = document.getElementById('root');
-if(rootEl && window.ReactDOM && window.DulceriaApp){
-  const root = ReactDOM.createRoot(rootEl);
-  root.render(React.createElement(window.DulceriaApp));
-}else{
-  console.error('Render error: falta ReactDOM o DulceriaApp');
-}
